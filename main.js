41bdc448f3a01ae9193020dd282f7c2c23ec5fa8
Arela-Cristina <instagrandine@gmail.com>
2024-10-14 16:04:10 +0200

    Add: second pseudocode reasoning

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-//Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
-//Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-//il prezzo del biglietto è definito in base ai km (0.21 € al km)
 //va applicato uno sconto del 20% per i minorenni
 //va applicato uno sconto del 40% per gli over 65.
 //L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
@@ -15,8 +12,13 @@ let km = parseInt(prompt('Type km numbers would you like to travel'));
 let kmNum = km;
 console.log (`This km numbers:  ${kmNum}`);
 
-//multiply the age of the passenger by the kilometers that want to travel
+//multiply the age of the passenger by the kilometers that want to travel (0.21 € by km)
 const kmRate = 0.21;
 let fullRate = kmNum * kmRate;
 console.log (`This full rate:  ${fullRate}`);
+
+//Conditions
+//impose a 20% discount for minor passenger  
+
+//impose a 40% discount for passengers over 65 years old
 
